cc419133245587a5305df559870e2c68db75b43c
hunterasdf <hunter6925@gmail.com>
2021-11-16 02:16:08 -0700

    ejs

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.render(pages/main)
+  res.render('home')
 })
 
 app.listen(PORT, () => {
